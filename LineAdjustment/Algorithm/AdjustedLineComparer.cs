91981f0aee752bfbf902d0973c765936eb5b217a
nSaytarov <rssnikki@gmail.com>
2022-05-23 12:38:49 +0500

    feat: add files

---
+++ LineAdjustment/Algorithm/AdjustedLineComparer.cs
@@ -0,0 +1,12 @@
+﻿namespace LineAdjustment.Algorithm;
+
+internal sealed class AdjustedLineComparer : IComparer<Line>
+{
+    public int Compare(Line x, Line y)
+    {
+        if (ReferenceEquals(x, y)) return 0;
+        if (ReferenceEquals(null, y)) return 1;
+        if (ReferenceEquals(null, x)) return -1;
+        return x.Index > y.Index ? 1 : -1;
+    }
+}
